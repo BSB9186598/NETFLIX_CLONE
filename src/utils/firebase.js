88bbe959a4f1bc70b3da4ed25500265b553f1876
BSB9186598 <bhaskarbhandari372@gmail.com>
2023-11-26 16:56:57 +0530

    changes

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,3 +1,28 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// import { getAuth } from "firebase/auth";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCX5lTkFRb7NnaTlYWkb17s2jIQIfP1vx8",
+//   authDomain: "personal-clone.firebaseapp.com",
+//   projectId: "personal-clone",
+//   storageBucket: "personal-clone.appspot.com",
+//   messagingSenderId: "536492184284",
+//   appId: "1:536492184284:web:c4c6d26c586d76d581da19",
+//   measurementId: "G-WX0CY3S4D8"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+// export const auth = getAuth();
+
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
@@ -8,13 +33,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC9PT67P9XXFeQ31tW7LxNERwuRUT0VPPc",
-  authDomain: "ppn-clone.firebaseapp.com",
-  projectId: "ppn-clone",
-  storageBucket: "ppn-clone.appspot.com",
-  messagingSenderId: "839848174955",
-  appId: "1:839848174955:web:f319c29f676f13ab02182e",
-  measurementId: "G-PVPQJP7NQQ"
+  apiKey: "AIzaSyCwEefhCHtGH605AJQVymuw5v92VarwATg",
+  authDomain: "pp-netflix-clone.firebaseapp.com",
+  projectId: "pp-netflix-clone",
+  storageBucket: "pp-netflix-clone.appspot.com",
+  messagingSenderId: "190875189965",
+  appId: "1:190875189965:web:556186e21efdc9e42e1f84",
+  measurementId: "G-51X8H22LGL"
 };
 
 // Initialize Firebase
